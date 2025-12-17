a693bb80ea17a02f6abf2d27fadea7036bf0c5e8
oZwZo <zhengwzh5@hotmail.com>
2025-12-17 23:20:46 +0800

    update yaml

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     
 
 setup(
-    name='pseudodyanmics',
+    name='pseudodynamics',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
